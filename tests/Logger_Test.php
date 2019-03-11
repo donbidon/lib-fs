404f714cad31dd3036e39808156a26aa07c0aaa5
don.bidon <don.bidon.dev@gmail.com>
2019-03-12 05:28:50 +0700

    Debugging remote UT...

--- tests/Logger_Test.php
+++ tests/Logger_Test.php
@@ -149,6 +149,7 @@ class Logger_Test extends \PHPUnit\Framework\TestCase
         if (file_exists($path)) {
             Tools::removeDir($path);
         }
+        fwrite(STDERR, "\n[\$path]\n\n");###
         mkdir($path, self::LOG_DIRECTORY_RIGHTS, TRUE);
     }
 
